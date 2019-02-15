b59d34d596e8cff6dce33f580ac9885deff8cc3b
Daniel Commins <dencee@users.noreply.github.com>
2019-02-15 14:53:39 -0800

    Dynamic Inteter array list done

--- src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
+++ src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
@@ -5,19 +5,52 @@ import java.util.ArrayList;
 public class _01_IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
+		ArrayList<String> s = new ArrayList<String>();
+		
 		//   Don't forget to import the ArrayList class
-
+		
 		//2. Add five Strings to your list
+		s.add("first string");
+		s.add("second string");
+		s.add("third string");
+		s.add("fourth string");
+		s.add("fifth string");
 		
 		//3. Print all the Strings using a standard for-loop
-		
+		System.out.println("Standard for loop:");
+		for( int i=0; i < s.size(); i++) {
+			System.out.println( s.get(i) );
+		}
 		
 		//4. Print all the Strings using a for-each loop
+		System.out.println("\nFor each loop:");
+		for( String eachStr : s ) {
+			System.out.println(eachStr);
+		}
 		
 		//5. Print only the even numbered elements in the list.
+		System.out.println("\nEven numbered elements:");
+		for( int i=0; i < s.size(); i+=1) {
+			if( i % 2 == 0 ) {
+				System.out.println(s.get(i));
+			}
+		}
 		
 		//6. Print all the Strings in reverse order.
+		System.out.println("\nReverse order");
+		for( int i = (s.size() - 1); i >= 0; --i ){
+			System.out.println(s.get(i));
+		}
 		
 		//7. Print only the Strings that have the letter 'e' in them.
+		String charToSearch = "d";
+		System.out.print("\nOnly strings containing " + charToSearch + ":");
+		for( int i=0; i < s.size(); ++i) {
+			String eachStr = s.get(i);
+			System.out.print("\n" + eachStr + ": ");
+			if( eachStr.contains(charToSearch) ) {
+				System.out.print( "contains " + charToSearch );
+			}
+		}
 	}
 }
--- src/_01_IntroToArrayLists/_02_GuestBook.java
+++ src/_01_IntroToArrayLists/_02_GuestBook.java
@@ -1,6 +1,75 @@
 package _01_IntroToArrayLists;
 
-public class _02_GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import intro_to_array_lists.GuestBook;
+
+public class _02_GuestBook implements ActionListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton nameButton = new JButton("Add Name");
+	JButton viewButton = new JButton("View Names");
+	JTextArea textField = new JTextArea(1, 40);
+	ArrayList<String> names = new ArrayList<String>();
+	
+	public static void main(String[] args) {
+		new _02_GuestBook().start();
+	}
+	
+	public void start() {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.add(panel);
+		frame.setTitle("Guest book");
+		
+		panel.add(nameButton);
+		panel.add(viewButton);
+		nameButton.addActionListener(this);
+		viewButton.addActionListener(this);
+		
+		panel.add(textField);
+		textField.setText("<descripton field>");
+		
+		frame.pack();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton clickedButton = (JButton)e.getSource();
+		
+		if( clickedButton == nameButton ) {
+			String newName = JOptionPane.showInputDialog("Please enter a name: ");
+			names.add(newName);
+			textField.setText("Added name: " + newName);
+		} else if( clickedButton == viewButton ) {
+			String namesList = "";
+			
+			if( names.size() <= 0 ) {
+				namesList = "There are no guests.";
+			} else {
+			
+				for( int i = 1; i <= names.size(); ++i ) {
+					namesList += "Guest #" + i + ": " + names.get(i - 1);
+					if( i != names.size() ) {
+						namesList += "\n";
+					}
+				}
+			}
+
+			textField.setText(namesList);
+			System.out.println(namesList);
+			frame.pack();
+		}
+	}
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
